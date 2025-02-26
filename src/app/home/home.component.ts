d536c1e26cd941ef624f3742c698bc9993650ca2
Solat Ali <solataliagha@gmail.com>
2025-02-26 19:56:27 +0500

    chore: cleanup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,14 @@
 import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   ElementRef,
-  OnInit,
+  NgZone,
   ViewChild,
-  inject,
+  inject
 } from '@angular/core';
-import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 import { ElectronService } from '../core/services';
-import { BehaviorSubject, map, of } from 'rxjs';
-import { NgZone } from '@angular/core';
-import { ChangeDetectorRef } from '@angular/core';
-import { ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -18,21 +16,15 @@ import { ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent implements OnInit {
-  electronService = inject(ElectronService);
-  ngZone = inject(NgZone);
+export class HomeComponent {
+  public electronService = inject(ElectronService);
   cdr = inject(ChangeDetectorRef);
+  ngZone = inject(NgZone);
 
   filePaths: string[] = [];
 
   @ViewChild('fileCtrl', { static: false }) fileCtrl!: ElementRef;
   loading$ = new BehaviorSubject<boolean>(false);
-
-  constructor(private router: Router) {}
-
-  ngOnInit(): void {
-    console.log('HomeComponent INIT');
-  }
 
   public onFileChange(fileChangeEvent: Event) {
     if (this.electronService.isElectron) {
@@ -40,10 +32,6 @@ export class HomeComponent implements OnInit {
       const files = (fileChangeEvent?.target as any)?.files as FileList;
       this.filePaths = Array.from(files).map((file) => file.path);
     }
-
-    //else {
-    //console.log('Sorry, you are in web...');
-    //}
   }
 
   public reset() {
@@ -53,12 +41,8 @@ export class HomeComponent implements OnInit {
 
   compress() {
     if (this.electronService.isElectron) {
-      //console.log(this.filePaths);
-
       this.loading$.next(true);
       this.electronService.ipcRenderer.send('compress-images', this.filePaths);  
-
-      //this.electronService.ipcRenderer.send('compress-images', this.filePaths);
 
       this.electronService.ipcRenderer.once(
         'compress-images-response',
@@ -77,23 +61,6 @@ export class HomeComponent implements OnInit {
           this.cdr.detectChanges();
         }
       );
-
-      // this.ngZone.run(() => {
-      //   this.electronService.ipcRenderer.once(
-      //     'compress-images-response',
-      //     (event: any, response: any) => {
-      //       console.log('Response from IPC: ', response);
-      //       if (response.success) {
-      //         console.log('Compression successful:', response.data);
-      //       } else {
-      //         console.error('Compression failed:', response.error);
-      //       }
-
-      //       this.loading$.next(false);
-      //       this.cdr.detectChanges();
-      //     }
-      //   );
-      // });
     }
   }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { HomeRoutingModule } from './home-routing.module';
-
 import { HomeComponent } from './home.component';
-import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule]
+  imports: [CommonModule, HomeRoutingModule]
 })
 export class HomeModule {}
